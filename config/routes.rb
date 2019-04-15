5f21f6d0e05bd31bdc501a54026299bdb0742d8e
Angela <angela.ohh@gmail.com>
2019-04-15 16:44:01 -0700

    added passengers/trips/drivers controller and fixed routes to each controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homepages#index"
-  get "hompages/passengers", to: "homepages#index", as: "all_passengers"
+
   resources :homepages
+  resources :passengers
+  resources :drivers
+  resources :trips
 end
