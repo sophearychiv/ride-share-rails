11cf6e3844ee4e5f0d882161c366e570d14088f8
Angela <angela.ohh@gmail.com>
2019-04-16 15:00:36 -0700

    added driver model method for all trips and average rating

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,4 +2,15 @@ class Driver < ApplicationRecord
   has_many :trips
   validates :name, presence: true
   validates :vin, presence: true, length: { in: 17..17 }
+
+  def all_trips
+    return all_trips = self.trips
+  end
+
+  def average_rating
+    all_ratings = all_trips.map do |trip|
+      trip.rating
+    end
+    return average_rating = (all_ratings.sum / all_ratings.length).to_i
+  end
 end
