720f52df392802780c6b940a17317fcc88555fb7
Angela <angela.ohh@gmail.com>
2019-04-16 15:34:14 -0700

    added edit and update method in passenger controller & updated show view

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -23,6 +23,23 @@ class PassengersController < ApplicationController
     end
   end
 
+  def edit
+    @passenger = Passenger.find_by(id: params[:id])
+  end
+
+  def update
+    @passenger = Passenger.find_by(id: params[:id])
+    is_successful = @passenger.update(passenger_params)
+    if is_successful
+      redirect_to passenger_path
+    else
+      render :edit, status: :bad_request
+    end
+  end
+
+  def delete
+  end
+
   private
 
   def passenger_params
