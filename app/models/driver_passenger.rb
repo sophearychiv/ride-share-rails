28d5742f7ae0838040cc3732c9fe6da9f5824eea
Sopheary Chiv <sophearychiv@gmail.com>
2019-04-16 11:32:23 -0700

    added validations to passenger and driver

--- app/models/driver_passenger.rb
+++
@@ -1,2 +0,0 @@
-class DriverPassenger < ApplicationRecord
-end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,6 @@
 class Passenger < ApplicationRecord
   has_many :trips
+  validates :phone_num, presence: true, uniqueness: true,
+                numericality: { only_integer: true }, length: 10
+  validates :name, presence: true
 end
