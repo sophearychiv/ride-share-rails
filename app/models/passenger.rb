1803c2baae4b60f45bae3fc61f3f10668973cae8
Angela <angela.ohh@gmail.com>
2019-04-16 14:40:26 -0700

    add feature to see passenger cost and passenger trip info

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,4 +2,16 @@ class Passenger < ApplicationRecord
   has_many :trips
   validates :phone_num, presence: true, uniqueness: true
   validates :name, presence: true
+
+  def all_trips
+    return all_trips = self.trips
+  end
+
+  def total_cost
+    total_cost = 0.0
+    all_trips.each do |trip|
+      total_cost += trip.cost
+    end
+    return total_cost
+  end
 end
