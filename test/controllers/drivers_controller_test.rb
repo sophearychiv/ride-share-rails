9bd2d853d025793ce50290ecdc398971f03b3eca
Sopheary Chiv <sophearychiv@gmail.com>
2019-04-16 17:03:10 -0700

    wrote index test for drivers_controller

--- test/controllers/drivers_controller_test.rb
+++ test/controllers/drivers_controller_test.rb
@@ -1,14 +1,20 @@
 require "test_helper"
 
 describe DriversController do
+  let (:driver) {
+    Driver.create name: "Jane Doe", vin: "12345678901234567"
+  }
   describe "index" do
     it "can get index" do
       # Your code here
+      get drivers_path
+      must_respond_with :success
     end
   end
 
   describe "show" do
     # Your tests go here
+
   end
 
   describe "edit" do
