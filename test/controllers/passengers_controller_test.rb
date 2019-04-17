e640d2f28d33c13bf41df60ca397229a5826fc41
Angela <angela.ohh@gmail.com>
2019-04-16 17:01:16 -0700

    added test for passenger controller new, index, and show actions

--- test/controllers/passengers_controller_test.rb
+++ test/controllers/passengers_controller_test.rb
@@ -1,16 +1,34 @@
 require "test_helper"
 
 describe PassengersController do
+  let(:passenger) {
+    Passenger.create name: "sample name", phone_num: "123", trip_id: nil
+  }
+  let(:invalid_id) {
+    "INVALID ID"
+  }
   describe "index" do
     it "can get index for all passengers" do
       get passengers_path
       must_respond_with :success
     end
-    # Your tests go here
+
+    it "can get to the root path" do
+      get root_path
+      must_respond_with :success
+    end
   end
 
   describe "show" do
-    # Your tests go here
+    it "can get a valid passenger" do
+      get passenger_path(passenger.id)
+      must_respond_with :success
+    end
+
+    it "will redirect for an invalid passenger" do
+      get passenger_path(invalid_id)
+      must_respond_with :redirect
+    end
   end
 
   describe "edit" do
@@ -22,11 +40,22 @@ describe PassengersController do
   end
 
   describe "new" do
-    # Your tests go here
+    it "can get to the new task page" do
+      get new_passenger_path
+      must_respond_with :success
+    end
   end
 
   describe "create" do
-    # Your tests go here
+    it "can create a new task" do
+      passenger_hash = {
+        passenger: {
+          name: "new passenger",
+          phone_num: "321",
+          trip_id: nil,
+        },
+      }
+    end
   end
 
   describe "destroy" do
