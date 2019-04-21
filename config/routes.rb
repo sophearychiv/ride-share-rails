cbef90e3424f2ffb454dcd987ff80cc1b1ed78b0
Sopheary Chiv <sophearychiv@gmail.com>
2019-04-20 23:12:10 -0700

    took off index route in TripsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homepages#index"
-  resources :trips
+  resources :trips, except: [:index]
 
   # resources :homepages
   resources :passengers do
